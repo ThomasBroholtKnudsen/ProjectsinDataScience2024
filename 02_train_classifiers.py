27ae16218ff54b34df2022f3f1e7903de2972779
LJONeill <ljon.london@gmail.com>
2024-05-11 14:03:16 +0200

    train_classifiers updated
    Saves the best classifier via pickle

--- 02_train_classifiers.py
+++ 02_train_classifiers.py
@@ -2,157 +2,87 @@
 import os
 import pandas as pd
 import numpy as np
-
-# Default packages for the minimum example
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GroupKFold
-from sklearn.metrics import accuracy_score #example for measuring performance
-
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
 
 import pickle #for saving/loading trained classifiers
 
 
-#Where are the files
-file_data = '..' + os.sep + 'data' + os.sep +'metadata.csv'
+# Load data
+file_data = 'data' + os.sep + 'okapi_data_metadata.csv'
 df = pd.read_csv(file_data)
-label = np.array(df['diagnostic'])
 
-# Creating a binary array corresponding to cancer/non-cancer (1 = cancer)
+# Prepare labels
+label = np.array(df['diagnosis'])
 cancer = np.copy(label)
-cancer[cancer == "BSS"] = 1
+cancer[cancer == "BCC"] = 1
 cancer[cancer == "MEL"] = 1
 cancer[cancer == "SCC"] = 1
 cancer[cancer == "ACK"] = 0
 cancer[cancer == "NEV"] = 0
 cancer[cancer == "SEK"] = 0
+cancer = cancer.astype(int)  # Ensure that labels are integers
 
 
-#Where did we store the features?
-file_features = 'features/features.csv'
-feature_names = ['asymmetry','colour','dots']
+# Load features
+annotation_data = 'features' + os.sep + 'features_okapi_images.csv'
+our_annotations = pd.read_csv(annotation_data)
+features = our_annotations[['asymmetry', 'colour', 'dots']].values
 
-# Load the features - remember the example features are not informative
-df_features = pd.read_csv(file_features)
+# Split data for training and testing (final evaluation)
+X_train, X_test, y_train, y_test = train_test_split(features, cancer, test_size=0.2, stratify=cancer)
 
+#Kfolding, and running on all models
+num_folds = 10
+num_classifiers = 3
+kfold = KFold(n_splits = num_folds)
 
-# Make the dataset, you can select different classes (see task 0)
-x = np.array(df_features[feature_names])
-y =  label == 'NEV'   #now True means healthy nevus, False means something else
-patient_id = df['patient_id']
-
-
-#Prepare cross-validation - images from the same patient must always stay together
-num_folds = 5
-group_kfold = GroupKFold(n_splits=num_folds)
-group_kfold.get_n_splits(x, y, patient_id)
-
-
-#Different classifiers to test out
-classifiers = [
-    KNeighborsClassifier(1),
-    KNeighborsClassifier(5)
-]
-num_classifiers = len(classifiers)
-
-      
 acc_val = np.empty([num_folds,num_classifiers])
 
-for i, (train_index, val_index) in enumerate(group_kfold.split(x, y, patient_id)):
-    
-    x_train = x[train_index,:]
-    y_train = y[train_index]
-    x_val = x[val_index,:]
-    y_val = y[val_index]
-    
-    
-    for j, clf in enumerate(classifiers): 
-        
-        #Train the classifier
-        clf.fit(x_train,y_train)
-    
-        #Evaluate your metric of choice (accuracy is probably not the best choice)
-        acc_val[i,j] = accuracy_score(y_val, clf.predict(x_val))
-   
-    
+for i_data, (train_index, val_index) in enumerate(kfold.split(X_train,y_train)):
+    #the number of training data sets created will therefore also be according to the num_folds
+    x_variant_train = X_train[train_index,:]
+    y_variant_train = y_train[train_index]
+    x_variant_val = X_train[val_index,:]
+    y_variant_val = y_train[val_index]
+
+    dec_tree.fit(x_variant_train, y_variant_train)
+    y_tree_prediction = dec_tree.predict(x_variant_val)
+    acc_val[i_data,0] = balanced_accuracy_score(y_variant_val, y_tree_prediction)
+
+    knn1 = KNeighborsClassifier(n_neighbors=1)
+    knn1.fit(x_variant_train, y_variant_train)
+    y_pred_knn1 = knn1.predict(x_variant_val)
+    bal_acc_knn1 = balanced_accuracy_score(y_variant_val, y_pred_knn1)
+    acc_val[i_data,1] = bal_acc_knn1
+
+    knn5 = KNeighborsClassifier(n_neighbors=5)
+    knn5.fit(x_variant_train, y_variant_train)
+    y_pred_knn5 = knn5.predict(x_variant_val)
+    bal_acc_knn5 = balanced_accuracy_score(y_variant_val, y_pred_knn5)
+    acc_val[i_data,2] = bal_acc_knn5
+
 #Average over all folds
 average_acc = np.mean(acc_val,axis=0) 
-   
-print('Classifier 1 average accuracy={:.3f} '.format(average_acc[0]))
-print('Classifier 2 average accuracy={:.3f} '.format(average_acc[1]))
+
+#The best classifier is.....
+if average_acc[0] > average_acc[1] and average_acc[0] > average_acc[2]:
+    classifier = DecisionTreeClassifier()
+elif average_acc[1] > average_acc[0] and average_acc[1] > average_acc[2]:
+    classifier = KNeighborsClassifier(1)
+else:
+    classifier = KNeighborsClassifier(5)
 
 
-
-#Let's say you now decided to use the 5-NN 
-classifier = KNeighborsClassifier(n_neighbors = 5)
-
-#It will be tested on external data, so we can try to maximize the use of our available data by training on 
-#ALL of x and y
-classifier = classifier.fit(x,y)
+#training this model on our entire dataset
+classifier = classifier.fit(features, cancer)
 
 #This is the classifier you need to save using pickle, add this to your zip file submission
-filename = 'groupXY_classifier.sav'
+filename = 'groupOkapi_classifier.sav'
 pickle.dump(classifier, open(filename, 'wb'))
-
-##Draft for the KNN code:
-
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
-
-# Split the data into training, validation, and test sets
-features = df[['asymmetry', 'colour', 'dots']]
-target = df['cancerous']
-
-x_train, x_val, y_train, y_val = train_test_split(features, target, test_size=0.2, stratify=target)
-x_val, x_test, y_val, y_test = train_test_split(x_val, y_val, test_size=0.5, stratify=y_val)
-
-# Train KNeighborsClassifier with different values of K
-knn_1 = KNeighborsClassifier(n_neighbors=1)
-knn_1.fit(x_train, y_train)
-
-knn_5 = KNeighborsClassifier(n_neighbors=5)
-knn_5.fit(x_train, y_train)
-
-# Predict on the validation set
-predict_knn1 = knn_1.predict(x_val)
-predict_knn5 = knn_5.predict(x_val)
-
-# Calculate accuracy and AUC scores
-acc_knn1 = accuracy_score(y_val, predict_knn1)
-acc_knn5 = accuracy_score(y_val, predict_knn5)
-auc_knn1 = roc_auc_score(y_val, knn_1.predict_proba(x_val)[:, 1])
-auc_knn5 = roc_auc_score(y_val, knn_5.predict_proba(x_val)[:, 1])
-
-results_without_random_state = {
-    "Accuracy KNN_1": acc_knn1,
-    "Accuracy KNN_5": acc_knn5,
-    "AUC KNN_1": auc_knn1,
-    "AUC KNN_5": auc_knn5
-}
-
-# Repeat with a fixed random state for reproducibility
-x_train, x_val, y_train, y_val = train_test_split(features, target, test_size=0.2, stratify=target, random_state=1907)
-x_val, x_test, y_val, y_test = train_test_split(x_val, y_val, test_size=0.5, stratify=y_val, random_state=1907)
-
-knn_1.fit(x_train, y_train)
-knn_5.fit(x_train, y_train)
-predict_knn1 = knn_1.predict(x_val)
-predict_knn5 = knn_5.predict(x_val)
-
-acc_knn1 = accuracy_score(y_val, predict_knn1)
-acc_knn5 = accuracy_score(y_val, predict_knn5)
-auc_knn1 = roc_auc_score(y_val, knn_1.predict_proba(x_val)[:, 1])
-auc_knn5 = roc_auc_score(y_val, knn_5.predict_proba(x_val)[:, 1])
-
-results_with_random_state = {
-    "Accuracy KNN_1": acc_knn1,
-    "Accuracy KNN_5": acc_knn5,
-    "AUC KNN_1": auc_knn1,
-    "AUC KNN_5": auc_knn5
-}
-
-results_without_random_state, results_with_random_state
-
 
 
 
